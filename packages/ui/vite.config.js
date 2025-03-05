6caf6ec613b3fb2beca34da81914cf26e98026c7
dhirenM <dhiren.mistry@yardi.com>
2025-03-05 12:15:03 -0800

    update base pa

--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig(async ({ mode }) => {
     dotenv.config()
     return {
         plugins: [react()],
+        base: '/virutosoconductornet/flowise/',
         resolve: {
             alias: {
                 '@': resolve(__dirname, 'src')
